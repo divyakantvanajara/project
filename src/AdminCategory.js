de657dfd3af6c71f498548082441bdf22e2addbb
divyakantvanajara <divyakant.1452@gmail.com>
2023-05-02 16:16:54 +0530

    category product fetch

--- src/AdminCategory.js
+++ src/AdminCategory.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
-class AdminCategory extends Component {
-    render() {
+class AdminCategory extends Component
+{
+   constructor(props)
+   {
+      super(props);
+      this.state = {
+        categories : []
+      };
+
+   }
+    componentDidMount()
+    {
+        console.log('componentDidMount called...');
+        let url = 'https://theeasylearnacademy.com/shop/ws/category.php';
+        fetch(url).then((response) => response.json()).then((json) =>
+        {
+            console.log(json);  
+            /*
+            [
+              0 {"error":"no"},
+              1{"total":3},
+              2{"id":"71","title":"test","photo":"C:fakepathforgot_password.png"},
+              3{"id":"54","title":"wubiwakiky@mailinator.com","photo":"undefined"},
+              4{"id":"74","title":"new","photo":"undefined"}
+            ] */
+            var error = json[0]['error'];
+            console.log(error);
+            if(error !== "no") //means error
+            {
+                alert(error);
+            }
+            else 
+            {
+              var total = json[1]['total'];
+              console.log(total);
+              json.splice(0, 2); //1st argument position, 2nd argument no of items to be removed from json
+              console.log('after removing item from array',json);
+              this.setState({
+                categories: json,
+              });
+            }
+            
+        });
+    }
+    render()
+    {
         return (<div className="container">
         <div className="row mt-5">
           <div className="col-12">
             <div className="h1 border-bottom pb-2 mb-2">Category Management</div>
             <p className="text-end">
-              <a href="/admin_insert_category" className="btn btn-primary">Add Category</a>
+              <a href="/admin-insert-category" className="btn btn-primary">Add Category</a>
             </p>
             <div className="card">
               <div className="card-header text-bg-primary">
@@ -18,33 +62,33 @@ class AdminCategory extends Component {
                       <th width="8%">Sr No</th>
                       <th>Title</th>
                       <th>Photo</th>
-                      <th width="8%">Is Live</th>
+                      
                       <th width="8%">Edit</th>
                       <th width="8%">Delete</th>
                     </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>Mobile</td>
-                      <td>
-                        <img src="https://picsum.photos/100" className="img-fluid" />
-                      </td>
-                      <td>
-                        Yes
-                      </td>
-                      <td>
-                        <a href="/admin_edit_category"><i className="fa fa-pencil fa-2x" /></a>
-                      </td>
-                      <td>
-                        <a href="#"><i className="fa fa-trash fa-2x" /></a>
-                      </td>
-                    </tr>
+                    {this.state.categories.map(function(category){
+                        return (<tr>
+                          <td>{category['id']}</td>
+                          <td>{category['title']}</td>
+                          <td>
+                            <img src="https://picsum.photos/100" className="img-fluid" />
+                          </td>
+                        
+                          <td>
+                            <a href="/admin-edit-category"><i className="fa fa-pencil fa-2x" /></a>
+                          </td>
+                          <td>
+                            <a href="#"><i className="fa fa-trash fa-2x" /></a>
+                          </td>
+                        </tr>)
+                    })}
                   </tbody></table>
               </div>
             </div>
           </div>
         </div>
       </div>
-      )
+      );
     }
 }
 export default AdminCategory
--- src/AdminProduct.js
+++ src/AdminProduct.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 class AdminProduct extends Component
 {
-    render(){
-        return(<div className="container">
+    constructor(props)
+    {
+      super(props);
+      console.log('constructor');
+      this.state = {
+        products : []
+      }
+    }
+    componentDidMount(){
+      console.log('componentDidMount');
+      var url = "https://theeasylearnacademy.com/shop/ws/product.php";
+      fetch(url).then((response) => response.json()).then((json)=>{
+        console.log(json);
+        /*
+          [
+              0 {"error":"no"},
+              1 {"total":2},
+              2 {"id":"2","categoryid":"1","title":"dell laptop","price":"200","stock":"100","weight":"3500","size":"15 inch","photo":"dell.jpg","detail":"WINDOWS 10 8 GB DDR3 RAM 512 gb ssd hard disk","islive":"1","isdeleted":"0"},
+              3 {"id":"42","categoryid":"0","title":"Apple","price":"1111","stock":"11","weight":"0","size":"","photo":"apple.jpg","detail":"All about apple","islive":"1","isdeleted":"0"}
+            ]
+        */
+            var error = json[0]['error'];
+            if(error !== "no")
+            {
+                alert(error);
+            }
+            else 
+            {
+              var total = json[1]['total'];
+              console.log(error,total);
+              json.splice(0,2);
+              console.log(json);
+              this.setState({
+                products:json
+              });
+            }
+      });
+    }
+    render()
+    {
+        return (<div className="container">
         <div className="row mt-5">
           <div className="col-12">
             <div className="h1 border-bottom pb-2 mb-2">Product Management</div>
             <p className="text-end">
-              <a href="/admin_insert_product" className="btn btn-primary">Add Product</a>
+              <a href="/admin-insert-product" className="btn btn-primary">Add Product</a>
             </p>
             <div className="card">
               <div className="card-header text-bg-primary">
@@ -17,43 +56,46 @@ class AdminProduct extends Component
                 <table className="table table-striped table-bordered">
                   <tbody><tr>
                       <th width="8%">Sr No</th>
+                      <th width="10%">Category</th>
                       <th width="20%">Title</th>
                       <th>Photo</th>
                       <th width="8%">Price</th>
                       <th width="8%">Stock</th>
                       <th>Detail</th>
-                      <th width="8%">Edit</th>
-                      <th width="8%">Delete</th>
+                      <th width="8%">Operation</th>
                     </tr>
-                    <tr>
-                      <td>1</td>
-                      <td>IPhone -13</td>
-                      <td>
-                        <img src="https://picsum.photos/100" className="img-fluid" />
-                      </td>
-                      <td>
-                        125000
-                      </td>
-                      <td>
-                        25
-                      </td>
-                      <td>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eligendi fuga commodi delectus aliquid, ullam consequuntur esse illum temporibus nobis laboriosam veritatis quo voluptas, assumenda, sunt impedit error voluptates ab.
-                      </td>
-                      <td>
-                        <a href="/admin_edit_product"><i className="fa fa-pencil fa-2x" /></a>
-                      </td>
-                      <td>
-                        <a href="#"><i className="fa fa-trash fa-2x" /></a>
-                      </td>
-                    </tr>
+                    {this.state.products.map(function(product){
+                      return( <tr>
+                        <td>1</td>
+                        <td>Mobile</td>
+                        <td>IPhone -13</td>
+                        <td>
+                          <img src="https://picsum.photos/100" className="img-fluid" />
+                        </td>
+                        <td>
+                          125000
+                        </td>
+                        <td>
+                          25
+                        </td>
+                        <td>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eligendi fuga commodi delectus aliquid, ullam consequuntur esse illum temporibus nobis laboriosam veritatis quo voluptas, assumenda, sunt impedit error voluptates ab.
+                        </td>
+                        <td>
+                          <a href="#"><i className="fa fa-trash fa-2x" /></a>
+                          <a href="/admin-edit-product"><i className="fa fa-pencil fa-2x" /></a>
+                        </td>
+                      </tr>)
+                    
+                    })}
+                   
                   </tbody></table>
               </div>
             </div>
           </div>
         </div>
       </div>
-      )
+      );
     }
 }
 export default AdminProduct
